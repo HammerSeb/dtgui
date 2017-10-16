2012fcfb10a7aad27468dbea73af30806e91e762
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-10-16 10:20:25 -0400

    Version bump to 1.0.1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
         license = 'MIT',
         url = URL,
         download_url = DOWNLOAD_URL,
-        version = '1.0.0',
+        version = '1.0.1',
         author = AUTHOR,
         author_email = AUTHOR_EMAIL,
         maintainer = AUTHOR,
